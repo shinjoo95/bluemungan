038cd0f8957d61882dab2ccd844ed4f26bc5d34b
shinjoo95 <shinjoo95@naver.com>
2024-05-20 15:03:37 +0900

    - 메인 롤링 배너 이미지 변경

--- lib/main/widget/rolling_banner.dart
+++ lib/main/widget/rolling_banner.dart
@@ -1,7 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class RollingBanner extends StatelessWidget {
   const RollingBanner({super.key});
@@ -10,27 +9,19 @@ class RollingBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     List imgItems = [
       _banner(
-          'assets/instagram_logo.png',
-          'assets/instagram_background.png',
-          Colors.white,
-          '푸른문간 활동이 궁금하다면?',
-          '푸른문간 인스타그램 바로가기 🙋🏻‍♂️',
-          'https://www.instagram.com/bluemungan?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='),
-      _banner(
-        'assets/mungan_logo.png',
-        'assets/mungan_background.png',
-        Colors.black,
-        '청년문간이 궁금하다면?',
-        '청년문간 홈페이지 바로가기 🙋🏻‍♂️',
-        'https://youthmungan.com/',
+        'assets/mungan_img.png',
       ),
       _banner(
-        'assets/youtube_logo.png',
-        'assets/youtube_background.png',
-        Colors.black,
-        '문간의 소식',
-        '청년문간 유튜브 바로가기 🙋🏻‍♂️',
-        'https://www.youtube.com/channel/UCkdAbucoJ5MFkbwPj8dsXwQ',
+        'assets/main_jeju_sea.jpeg',
+      ),
+      _banner(
+        'assets/main_inwang.jpeg',
+      ),
+      _banner(
+        'assets/main_jeju.jpeg',
+      ),
+      _banner(
+        'assets/main_roma.jpeg',
       ),
     ];
     return CarouselSlider(
@@ -53,64 +44,23 @@ class RollingBanner extends StatelessWidget {
 }
 
 Widget _banner(
-  String logoImage,
   String backgroundImage,
-  Color textColor,
-  String subText,
-  String mainText,
-  String urlLink,
 ) {
-  return GestureDetector(
+  return Container(
+    width: Get.width,
+    margin: const EdgeInsets.symmetric(horizontal: 5), //좌우 여백
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(5),
+    ),
     child: Container(
-      width: Get.width,
-      margin: const EdgeInsets.symmetric(horizontal: 5), //좌우 여백
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(backgroundImage), // 배경 이미지
-          ),
-        ),
-        padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              logoImage,
-              width: 30,
-              height: 30,
-            ),
-            const SizedBox(height: 10),
-            // 소 제목
-            Text(
-              subText,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 17,
-              ),
-            ),
-            // 대 제목
-            Text(
-              mainText,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 23,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            // 인디게이터
-          ],
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(backgroundImage), // 배경 이미지
         ),
       ),
+      padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
     ),
-    onTap: () {
-      launchUrl(
-        Uri.parse(urlLink),
-      );
-    },
   );
 }
