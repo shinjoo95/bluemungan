760115cc2c6df7248b876a64bac72a70a2a4f943
shinjoo95 <shinjoo95@naver.com>
2024-05-23 19:40:23 +0900

    - 카카오 firebase 연동

--- lib/my/screen/login_screen.dart
+++ lib/my/screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bluemungan/main/screen/main_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
@@ -27,62 +28,7 @@ class LoginScreen extends StatelessWidget {
               textColor: Colors.black,
               buttonColor: const Color(0xffffde00),
               onTap: () async {
-                if (await isKakaoTalkInstalled()) {
-                  try {
-                    await UserApi.instance.loginWithKakaoTalk();
-                    print('카카오톡으로 로그인 성공111');
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                          builder: (context) => const MainScreen()),
-                      (route) => false,
-                    );
-                  } catch (error) {
-                    print('카카오톡으로 로그인 실패 $error');
-                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                    if (error is PlatformException &&
-                        error.code == 'CANCELED') {
-                      return;
-                    }
-                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-                    try {
-                      await UserApi.instance.loginWithKakaoAccount();
-                      print('카카오계정으로 로그인 성공222');
-                      Navigator.of(context).pushAndRemoveUntil(
-                        MaterialPageRoute(
-                            builder: (context) => const MainScreen()),
-                        (route) => false,
-                      );
-                    } catch (error) {
-                      print('카카오계정으로 로그인 실패 $error');
-                    }
-                  }
-                } else {
-                  try {
-                    await UserApi.instance.loginWithKakaoAccount();
-                    print('카카오계정으로 로그인 성공333');
-                    try {
-                      User user = await UserApi.instance.me();
-                      print(
-                          'shin >>>> ${user.kakaoAccount?.profile?.profileImageUrl}');
-
-                      print('사용자 정보 요청 성공'
-                          '\n회원번호: ${user.id}'
-                          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
-                          '\n이메일: ${user.kakaoAccount?.email}');
-                    } catch (error) {
-                      print('사용자 정보 요청 실패 $error');
-                    }
-
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                          builder: (context) => const MainScreen()),
-                      (route) => false,
-                    );
-                  } catch (error) {
-                    print('카카오계정으로 로그인 실패 $error');
-                  }
-                }
+                await loginWithKakao();
               },
             ),
             _loginBotton(
@@ -155,4 +101,39 @@ Widget _loginBotton({
       const SizedBox(height: 15),
     ],
   );
+}
+
+Future<void> loginWithKakao() async {
+  // 카카오톡 설치 여부 확인
+  // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+  if (await isKakaoTalkInstalled()) {
+    try {
+      var provider = OAuthProvider("oidc.bluemungan");
+      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+      var credential = provider.credential(
+        idToken: token.idToken,
+        accessToken: token.accessToken,
+      );
+      FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (error) {
+      // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+      // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+      if (error is PlatformException && error.code == 'CANCELED') {
+        return;
+      }
+      // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  } else {
+    try {
+      await UserApi.instance.loginWithKakaoAccount();
+      print('카카오계정으로 로그인 성공');
+    } catch (error) {
+      print('카카오계정으로 로그인 실패 $error');
+    }
+  }
 }
