1fbcc54446c787a3cd8e1a24c3aa35b3e541bf25
shinjoo95 <shinjoo95@naver.com>
2024-06-12 17:06:29 +0900

    - AOS apk 추출 수정

--- android/app/src/main/kotlin/com/example/bluemungan/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bluemungan/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.bluemungan
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity()
